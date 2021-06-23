3d2deb6f3738e50f56a15be0c7189e9983ef190a
zouhy <zouhangyu@buffalo-robot.com>
2021-06-23 23:22:43 +0800

    aa

--- bfrredis_test.go
+++ bfrredis_test.go
@@ -142,7 +142,7 @@ func TestBFRRedis_Create(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if err := tt.args.query.Create(context.Background(), tt.args.v); err != nil {
+			if err := tt.args.query.SubModel(true).Create(context.Background(), tt.args.v); err != nil {
 				fmt.Println(err)
 			}
 		})
--- go.mod
+++ go.mod
@@ -1,4 +1,4 @@
-module gogs.bfr.com/zouhy/rorm
+module gogs.buffalo-robot.com/zouhy/rorm
 
 go 1.15
 
@@ -6,6 +6,5 @@ require (
 	github.com/go-redis/redis/v8 v8.4.0
 	github.com/stretchr/testify v1.6.1
 	go.uber.org/zap v1.16.0
-	gogs.bfr.com/zouhy/micro v0.0.0-20210321074058-1f31b2521938
-
+	
 )
